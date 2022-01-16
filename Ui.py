d3d2f3a3b0f167cdcc45d55eb35c7a97cc55f4e0
Omar Garza Cantu <garzacao@oregonstate.edu>
2022-01-15 18:53:28 -0600

    testing phase.

--- Ui.py
+++ Ui.py
@@ -1,11 +1,10 @@
 
 import time
 
-
-
-print("Please enter 1 to display a random image\n Enter 2 to end the program!")
-choice = input()
 action_word = "run"
+
+print("Please enter 1 to display a random image file path\n")
+choice = int(input("Your choice: "))
 
 while True:
 
@@ -14,10 +13,8 @@ while True:
         file.write(action_word)
         file.close()
 
-        time.sleep(5)
+        time.sleep(20)
 
         file = open("image-service.txt", "r")
         message = file.read()
         file.close()
-
-        print()
--- imgsrv.py
+++ imgsrv.py
@@ -1,19 +1,29 @@
-
+import os
 import time
 
 image_elements = 5
 
 while True:
 
-    time.sleep(1)
+    time.sleep(5)
     file = open("image-service.txt")
-    message = int(file.read())
+
+    try:
+        message = int(file.read())
+
+    except:
+        continue
+
     file.close()
 
     image_num = message % image_elements
-    image_path = "C:\\Users\\Omar\\PycharmProjects\\CS361-Assign1\\" + str(image_num) + ".jpg"
+    file_names = os.listdir("images")
+
+    image_path = os.getcwd() + "\\images\\" + file_names[0]
 
     file = open("image-service.txt", "w")
     file.write(image_path)
     file.close()
+
+
 
